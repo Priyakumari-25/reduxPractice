76f3e3c5b2973190fde866866d0f4b626918a569
Priyakumari-25 <priya@peoplelinvc.com>
2023-08-28 15:14:11 +0530

    add ui and folder structure

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
+        <div></div>
       <h1>home</h1>
       <div className="cart-wrapper">
         <div className=" img-wrapper item">
